aaa9fdebcd4c92d5810fb6ca59ff5f9c404e8da8
Caique <caiquemorales20@gmail.com>
2023-07-30 14:20:50 -0300

    :sparkles: feat: Next 13

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "Light Films",
+	description:
+		"Descubra os incríveis benefícios da aplicação de insulfilm em sua casa, comércio ou escritório. Reduza o calor, bloqueie os raios UV prejudiciais, aumente a privacidade e fortaleça a segurança de suas janelas. Transforme seu espaço em um ambiente mais confortável, protegido e eficiente com o insulfilm. Saiba mais!",
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
+}
