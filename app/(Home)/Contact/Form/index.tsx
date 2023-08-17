204a82f6842099b9e82693d11a6543b9d67a4a13
Caique <caiquemorales20@gmail.com>
2023-08-16 22:20:41 -0300

    :construction: Email sucess modal

--- app/(Home)/Contact/Form/index.tsx
+++ app/(Home)/Contact/Form/index.tsx
@@ -1,13 +1,18 @@
+// Client Side
 "use client";
 
 // Imported Components
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
+import Modal from "../Modal";
 
 // Functional Component
 export default function Form() {
+	// Variables
+	const [modalOpened, setModalOpened] = useState(false);
 	const form = useRef<any>();
 
+	// Functions
 	const sendEmail = (e: any) => {
 		e.preventDefault();
 		emailjs
@@ -29,33 +34,44 @@ export default function Form() {
 
 	// Rendering
 	return (
-		<form
-			ref={form}
-			onSubmit={sendEmail}
-			className="flex flex-col p-8 bg-primary-400 rounded-lg"
-		>
-			<label className="text-white mb-2 md:mb-4 font-semibold">Nome</label>
-			<input
-				className="bg-white mb-4 md:mb-8 px-4 py-2 border-primary-400 border-2 rounded-md"
-				type="text"
-				name="user_name"
-			/>
-			<label className="text-white mb-2 md:mb-4 font-semibold">Email</label>
-			<input
-				className="bg-white mb-4 md:mb-8 px-4 py-2 border-primary-400 border-2 rounded-md"
-				type="email"
-				name="user_email"
-			/>
-			<label className="text-white mb-2 md:mb-4 font-semibold">Mensagem</label>
-			<textarea
-				className="mb-4 md:mb-8 px-4 py-2 h-44 bg-white border-primary-400 border-2 rounded-md resize-none"
-				name="message"
-			/>
-			<button className="align w-full md:w-max rounded-[1rem] border-solid bg-white border-[2px] hover:border-white hover:bg-primary-400 text-primary-400 hover:text-white duration-300">
-				<p className="text-sm font-bold uppercase p-[1.125em_5.125em]">
-					Enviar
-				</p>
-			</button>
-		</form>
+		<>
+			{modalOpened && <Modal />}
+			<form
+				ref={form}
+				onSubmit={sendEmail}
+				className="flex flex-col p-8 bg-primary-400 rounded-lg"
+			>
+				<label className="text-white mb-2 md:mb-4 font-semibold">Nome</label>
+				<input
+					className="bg-white mb-4 md:mb-8 px-4 py-2 border-primary-400 border-2 rounded-md"
+					type="text"
+					name="user_name"
+					required
+				/>
+				<label className="text-white mb-2 md:mb-4 font-semibold">Email</label>
+				<input
+					className="bg-white mb-4 md:mb-8 px-4 py-2 border-primary-400 border-2 rounded-md"
+					type="email"
+					name="user_email"
+					required
+				/>
+				<label className="text-white mb-2 md:mb-4 font-semibold">
+					Mensagem
+				</label>
+				<textarea
+					className="mb-4 md:mb-8 px-4 py-2 h-44 bg-white border-primary-400 border-2 rounded-md resize-none"
+					name="message"
+					required
+				/>
+				<button
+					onClick={() => setModalOpened(true)}
+					className="align w-full md:w-max rounded-[1rem] border-solid bg-white border-[2px] hover:border-white hover:bg-primary-400 text-primary-400 hover:text-white duration-300"
+				>
+					<p className="text-sm font-bold uppercase p-[1.125em_5.125em]">
+						Enviar
+					</p>
+				</button>
+			</form>
+		</>
 	);
 }
---
+++ app/(Home)/Contact/Modal/index.tsx
@@ -0,0 +1,11 @@
+// Functional Component
+export default function Modal() {
+	// Rendering
+	return (
+		<div className="bg-black/60 w-full h-full fixed flex items-center justify-center inset-0 z-50">
+			<div className="w-40 h-20 bg-white flex items-center justify-center">
+				<p className="text-primary-4009">lorem500</p>
+			</div>
+		</div>
+	);
+}
